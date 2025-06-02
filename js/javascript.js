30a470f72f7e45bc2d9e5c23326ed75e3b7b6d93
melnicuk <msamelnicuk@gmail.com>
2025-06-03 00:00:22 +0300

    Golovna chastina saitu

---
+++ js/javascript.js
@@ -0,0 +1,9 @@
+const toggle = document.getElementById('theme-toggle-checkbox');
+
+toggle.addEventListener('change', function () {
+  if (this.checked) {
+    document.body.style.backgroundColor = '#F9E1B5'; 
+  } else {
+    document.body.style.backgroundColor = '#7F3C25'; 
+  }
+});
